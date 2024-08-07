3641be63a025942a07a12d00802833f80ff948b8
Trishna <trishna.patil13@gmail.com>
2024-08-06 22:38:11 -0400

    using redux toolkit and createSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,38 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
 
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
 const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
+    state.counter++;
+    return state;
+
+    /*
     return {
       counter: state.counter + 1,
       showCounter: state.showCounter,
     };
+    */
   }
 
   if (action.type === 'increase') {
